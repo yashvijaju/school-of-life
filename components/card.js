cd94bf19c46dc182223159131f9a9e57895821fe
yjaju <yjaju@usc.edu>
2021-02-21 03:50:50 +0530

    updates

--- components/card.js
+++ components/card.js
@@ -23,8 +23,8 @@ const useStyles = makeStyles({
         textAlign: 'center',
     },
     container_cardonboard: {
-        width: 'calc(80vw / 8)',
-        height: 'calc(85vh / 5)',
+        width: 'calc(80vw / 10)',
+        height: 'calc(85vh / 6)',
         border: '2px solid black',
         borderColor: props => props.primaryColor,
         backgroundColor: props => props.secondaryColor,
@@ -45,6 +45,45 @@ const useStyles = makeStyles({
         bottom: '1vh',
         right: '1vw'
     },
+    container_cardinhand: {
+        width: 'calc(80vw / 10)',
+        height: 'calc(85vh / 6)',
+        border: '2px solid black',
+        borderColor: props => props.secondaryColor,
+        backgroundColor: props => props.primaryColor,
+        color: props => props.secondaryColor,
+        position: 'relative',
+    },
+    number_cardinhand: {
+        color: props => props.secondaryColor,
+        fontWeight: 'bold',
+        position: 'absolute',
+        top: '1vh',
+        left: '1vw'
+    },
+    icon_cardinhand: {
+        width: '50%',
+        height: 'auto',
+        position: 'absolute',
+        bottom: '1vh',
+        right: '1vw'
+    },
+    container_cardinhand_hover: {
+        width: 'calc(80vw / 10)',
+        height: 'calc(85vh / 6)',
+        border: '2px solid black',
+        borderColor: props => props.secondaryColor,
+        backgroundColor: props => props.primaryColor,
+        color: props => props.secondaryColor,
+        position: 'relative',
+        transition: '0.25s',
+        transform: 'scale(2)',
+        zIndex: 2,
+    },
+    question_cardinhand: {
+        padding: '0.5vh 0.5vw',
+        fontSize: '0.4rem',
+    }
 });
 
 export default function Card(props) {
@@ -79,5 +118,30 @@ export function CardOnBoard(props) {
             </Grid>
         </Grid>
     )
-  }
+}
+
+export function CardInHand(props) {
+    const styles = useStyles(props);
+    const [hover, changeHover] = React.useState(false)
+
+    return (
+        <div>
+            {!hover ?
+                <Grid className={styles.container_cardinhand} container direction="row" alignContent="space-between"  onMouseEnter={()=>changeHover(true)}>
+                    <Grid item xs={12} container justify="flex-start">
+                        <Typography className={styles.number_cardinhand} variant="h3">{props.number}</Typography>
+                    </Grid>
+                    <Grid item xs={12} container justify="flex-end">
+                        <img className={styles.icon_cardinhand} src={props.icon}/>
+                    </Grid>
+                </Grid>
+                :
+                <Grid className={styles.container_cardinhand_hover} container direction="column" alignItems="center" justify="center" onMouseLeave={()=>changeHover(false)}>
+                <p className={styles.question_cardinhand}>{props.question}</p>
+                </Grid>
+            }
+        </div>
+        
+    )
+}
   
--- pages/game.js
+++ pages/game.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { Typography, Grid, makeStyles } from '@material-ui/core'
 
 // components
-import Card, { CardOnBoard } from '../components/card'
+import Card, { CardOnBoard, CardInHand } from '../components/card'
 
 const red = '#EE3A20';
 const black = '#000000';
@@ -18,6 +18,22 @@ const useStyles = makeStyles({
         height: '2.5vh',
         width: 'auto',
         cursor: 'pointer'
+    },
+    layer: {
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    },
+    playerTurn: {
+        position: 'absolute',
+        top: 'calc((100vh - 400px)/2)',
+        left: 'calc((100vw - 500px)/2)',
+    },
+    activePlayerTurn: {
+        backgroundColor: '#32B1CC',
     }
 });
 
@@ -27,34 +43,69 @@ export default function Sequence() {
     const iconB = '/assets/cupid.svg';
     const iconC = '/assets/family.svg';
     const iconD = '/assets/18.svg';
+
+    const primaryColor="##32B1CC";
+    const secondaryColor="##EAEE20";
+
     const [board, setBoard] = React.useState([
-        [{number: '3', icon: iconA},{number: '3', icon: iconB},{number: '3', icon: iconC},{number: '3', icon: iconB},{number: '3', icon: iconA},{number: '3', icon: iconD},{number: '3', icon: iconB},{number: '3', icon: iconA}],
-        [{number: '3', icon: iconC},{number: '3', icon: iconA},{number: '3', icon: iconB},{number: '3', icon: iconD},{number: '3', icon: iconC},{number: '3', icon: iconA},{number: '3', icon: iconD},{number: '3', icon: iconC}],
-        [{number: '3', icon: iconB},{number: '3', icon: iconD},{number: '3', icon: iconC},{number: '3', icon: iconA},{number: '3', icon: iconB},{number: '3', icon: iconD},{number: '3', icon: iconC},{number: '3', icon: iconB}],
-        [{number: '3', icon: iconD},{number: '3', icon: iconA},{number: '3', icon: iconD},{number: '3', icon: iconC},{number: '3', icon: iconD},{number: '3', icon: iconA},{number: '3', icon: iconB},{number: '3', icon: iconA}],
-        [{number: '3', icon: iconC},{number: '3', icon: iconD},{number: '3', icon: iconB},{number: '3', icon: iconA},{number: '3', icon: iconC},{number: '3', icon: iconB},{number: '3', icon: iconC},{number: '3', icon: iconD}],
+        [{number: '1', icon: iconA},{number: '12', icon: iconB},{number: '14', icon: iconC},{number: '10', icon: iconB},{number: '9', icon: iconC},{number: '12', icon: iconD},{number: '13', icon: iconB},{number: '15', icon: iconA},{number: '11', icon: iconA},{number: '9', icon: iconB},],
+        [{number: '3', icon: iconC},{number: '10', icon: iconA},{number: '4', icon: iconB},{number: '9', icon: iconD},{number: '6', icon: iconC},{number: '13', icon: iconA},{number: '1', icon: iconD},{number: '8', icon: iconC},{number: '8', icon: iconB},{number: '7', icon: iconC},],
+        [{number: '5', icon: iconB},{number: '11', icon: iconD},{number: '1', icon: iconC},{number: '9', icon: iconA},{number: '3', icon: iconB},{number: '2', icon: iconD},{number: '13', icon: iconC},{number: '14', icon: iconB},{number: '10', icon: iconC},{number: '8', icon: iconD},],
+        [{number: '3', icon: iconD},{number: '2', icon: iconA},{number: '13', icon: iconD},{number: '15', icon: iconC},{number: '5', icon: iconD},{number: '3', icon: iconA},{number: '11', icon: iconB},{number: '8', icon: iconA},{number: '15', icon: iconD},{number: '4', icon: iconA},],
+        [{number: '4', icon: iconC},{number: '4', icon: iconD},{number: '1', icon: iconB},{number: '7', icon: iconA},{number: '12', icon: iconC},{number: '6', icon: iconB},{number: '2', icon: iconC},{number: '10', icon: iconD},{number: '12', icon: iconA},{number: '15', icon: iconB},],
+        [{number: '6', icon: iconD},{number: '5', icon: iconA},{number: '6', icon: iconA},{number: '2', icon: iconB},{number: '5', icon: iconC},{number: '7', icon: iconD},{number: '14', icon: iconA},{number: '7', icon: iconB},{number: '11', icon: iconC},{number: '14', icon: iconD},],
     ]);
+    const [playerCards, setPlayerCards] = React.useState(
+        [{number: '3', icon: iconA, question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?"},{number: '3', icon: iconB, question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor??"},{number: '3', icon: iconC, question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor??"},{number: '3', icon: iconB, question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?"},{number: '3', icon: iconA, question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?"}])
+    
+    
+    const [activePlayer, setActivePlayer] = React.useState("kavya")
 
     return(
         <div className={styles.container_outer}>
             <Link href="/">
                 <img className={styles.quit_icon} src="/assets/quit.svg"/>
             </Link>
-            <Grid container direction="column">
-                {board.map((array, index) => 
-                    <Grid container direction="row" key={index}>
-                        {array.map((obj, index_) => 
-                            <Grid item key={index_}>
-                                <CardOnBoard number={obj.number} icon={obj.icon} primaryColor="#32B1CC" secondaryColor="#EAEE20"/>
+            <Grid container direction="row">
+                <Grid container direction="column" item xs={11}>
+                    {board.map((array, index) => 
+                        <Grid container direction="row" key={index}>
+                            {array.map((obj, index_) => 
+                                <Grid item key={index_}>
+                                    <CardOnBoard number={obj.number} icon={obj.icon} primaryColor={primaryColor} secondaryColor={secondaryColor}/>
+                                </Grid>
+                            )}
+                        </Grid>
+                    )}
+                </Grid>
+                <Grid container direction="column" item xs={1}>
+                    <Grid item style={{height: 'calc(85vh / 6)'}}>
+                        <Typography variant="h6">
+                            my cards
+                        </Typography>
+                        <Typography variant="caption">
+                            ( hover over any card to see the question )
+                        </Typography>
+                    </Grid>
+                        {playerCards.map((obj, index) => 
+                            <Grid item key={index}>
+                                <CardInHand number={obj.number} icon={obj.icon} question={obj.question} primaryColor={primaryColor} secondaryColor={secondaryColor}/>
                             </Grid>
                         )}
-                    </Grid>
-                )}
-
+                </Grid>
             </Grid>
-            {/* <Grid className={styles.playerTurn}>
-                <Card primaryColor="#32B1CC" secondaryColor="#EAEE20" number="3" icon="/assets/friends.svg" question="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?"/>
-            </Grid> */}
+
+            <Grid className={styles.layer}>
+                <Grid className={styles.playerTurn}>
+                    <Grid className={styles.activePlayerTurn}>
+                        <Typography variant="h3">
+                            {activePlayer} picked
+                        </Typography>
+                    </Grid>
+                    <Card primaryColor="#32B1CC" secondaryColor="#EAEE20" number="3" icon="/assets/friends.svg" question="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?"/>
+                </Grid>
+            </Grid>
+            
         </div>
     )
 }
