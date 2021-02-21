d3a850bff14b7a2f40da5ed4caaa7958c05e8922
yjaju <yjaju@usc.edu>
2021-02-21 22:53:17 +0530

    push

--- pages/game.js
+++ pages/game.js
@@ -189,7 +189,7 @@ export default function Sequence(props) {
             
         }
         setQuestionCounter(20)
-    })
+    }, [])
 
     const [users, setUsers] = React.useState([
         {"_id": router.query.username, "team_id": 1, "cards_in_hand": user1_deck},
